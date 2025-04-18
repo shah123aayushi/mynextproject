21cec90bd53645bb6ff52340dd0e05cc7ae0e86d
Aayushi Shah <ayushishah37832@gmail.com>
2025-04-18 16:18:22 +0530

    errror

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
 import Link from "next/link"; // ✅ Don't forget this!
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// Fonts
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "My Website",
